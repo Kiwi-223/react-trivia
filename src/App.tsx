ae2ae8d3894c800ce0b4fa404164b6036df0dcd1
Kiwi-223 <149635096+Kiwi-223@users.noreply.github.com>
2024-06-04 17:52:08 -0600

    added Previous button funtion

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 // import triviaList from "./assets/triviaList";
 import { Question } from "./componants/Question";
+import { MoveButton } from "./componants/moveButton";
 
 function App() {
+  const [cardIndex, setCardIndex] = useState(2);
   return (
     <>
       <div className="mainHead">
         <h1> Trivia</h1>
       </div>
-      <div className="content">
-        <div className="card">
-          <Question qIndex={0}></Question>
 
+      <div className="content">
+        <MoveButton
+          name="Previous"
+          onClick={() => setCardIndex(cardIndex - 1)}
+        ></MoveButton>{" "}
+        {/*move to previous*/}
+        <div className="card">
+          <Question qIndex={cardIndex}></Question>
         </div>
+        {/* <MoveButton name="Next" onClick={}></MoveButton>  */}
+        {/*move to next*/}
       </div>
     </>
   );
---
+++ src/componants/moveButton.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+interface Props {
+    name: string;
+    // color?: string;
+    onClick: () => void;
+  }
+
+export const MoveButton = ({name, onClick}: Props) => {
+  return (
+    <button onClick={onClick}>{name}</button>
+  )
+}
