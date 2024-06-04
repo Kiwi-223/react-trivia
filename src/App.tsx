57b49eb865d10bda6f91f84043f38fcc8da13b82
Kiwi-223 <149635096+Kiwi-223@users.noreply.github.com>
2024-06-04 15:30:03 -0600

    Question card added with single question

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
+// import triviaList from "./assets/triviaList";
+import { Question } from "./componants/Question";
 
 function App() {
-
   return (
- <>
- <h1>Hello World</h1>
- </>
-  )
+    <>
+      <div className="mainHead">
+        <h1> Trivia</h1>
+      </div>
+      <div className="content">
+        <div className="card">
+          <Question qIndex={0}></Question>
+
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/assets/triviaList.tsx
+++ src/assets/triviaList.tsx
@@ -1,82 +1,97 @@
 // list array will all questions and answers for the trivia questions
-const trivaList = [
-    {
-        question: "Who teaches Mowgli about 'The Bare Necessities' in the Jungle Book?",
-        answer: "Baloo"
-      },
-      {
-        question: "Disney's Animal Kingdom theme park opened in Florida in which year?",
-        answer: "1998"
-      },
-      {
-        question: "Gaston is the villain in which Disney movie?",
-        answer: "Beauty and the Beast"
-      },
-      {
-        question: "What is the name of the family in Mary Poppins?",
-        answer: "Banks"
-      },
-      {
-        question: "What is the name of Buzz and Woody's original owner in Toy Story?",
-        answer: "Andy"
-      },
-      {
-        question: "Let it Go' is a song from which famous Disney animated movie?",
-        answer: "Frozen"
-      },
-      {
-        question: "Nemo ends up in a tank in which sort of office?",
-        answer: "Dentist"
-      },
-      {
-        question: "What type of animal was Robin in the animated version of Robin Hood?",
-        answer: "Fox"
-      },
-      {
-        question: "Disney opened its first European park close to which city in 1992?",
-        answer: "Paris"
-      },
-      {
-        question: "Who is the wisecracking genie in 'Aladdin'?",
-        answer: "Genie"
-      },
-      {
-        question: "Which Disney character is a talking candlestick in 'Beauty and the Beast'?",
-        answer: "Lumière"
-      },
-      {
-        question: "What is the name of the brave captain in 'Pirates of the Caribbean'?",
-        answer: "Captain Jack Sparrow"
-      },
-      {
-        question: "Which Disney character is known for his high-pitched voice and love for honey?",
-        answer: "Winnie the Pooh"
-      },
-      {
-        question: "In 'The Princess and the Frog,' what is the name of the trumpet-playing alligator?",
-        answer: "Louis"
-      },
-      {
-        question: "Who is the main antagonist in 'The Little Mermaid'?",
-        answer: "Ursula"
-      },
-      {
-        question: "Which Disney character is a brave and determined toy cowboy in the 'Toy Story' franchise?",
-        answer: "Woody"
-      },
-      {
-        question: "What is the name of the talking snowman in 'Frozen'?",
-        answer: "Olaf"
-      },
-      {
-        question: "Who is the adventurous young boy in the movie 'Up'?",
-        answer: "Russell"
-      },
-      {
-        question: "Which Disney character is a charismatic and mischievous pirate fox in 'Zootopia'?",
-        answer: "Nick Wilde"
-      }
-    ];
-    
-    // Export the array if using in a module-based environment
-    export default trivaList;
+const triviaList = [
+  {
+    question:
+      "Who teaches Mowgli about 'The Bare Necessities' in the Jungle Book?",
+    answer: "Baloo",
+  },
+  {
+    question:
+      "Disney's Animal Kingdom theme park opened in Florida in which year?",
+    answer: "1998",
+  },
+  {
+    question: "Gaston is the villain in which Disney movie?",
+    answer: "Beauty and the Beast",
+  },
+  {
+    question: "What is the name of the family in Mary Poppins?",
+    answer: "Banks",
+  },
+  {
+    question:
+      "What is the name of Buzz and Woody's original owner in Toy Story?",
+    answer: "Andy",
+  },
+  {
+    question: "Let it Go' is a song from which famous Disney animated movie?",
+    answer: "Frozen",
+  },
+  {
+    question: "Nemo ends up in a tank in which sort of office?",
+    answer: "Dentist",
+  },
+  {
+    question:
+      "What type of animal was Robin in the animated version of Robin Hood?",
+    answer: "Fox",
+  },
+  {
+    question:
+      "Disney opened its first European park close to which city in 1992?",
+    answer: "Paris",
+  },
+  {
+    question: "Who is the wisecracking genie in 'Aladdin'?",
+    answer: "Genie",
+  },
+  {
+    question:
+      "Which Disney character is a talking candlestick in 'Beauty and the Beast'?",
+    answer: "Lumière",
+  },
+  {
+    question:
+      "What is the name of the brave captain in 'Pirates of the Caribbean'?",
+    answer: "Captain Jack Sparrow",
+  },
+  {
+    question:
+      "Which Disney character is known for his high-pitched voice and love for honey?",
+    answer: "Winnie the Pooh",
+  },
+  {
+    question:
+      "In 'The Princess and the Frog,' what is the name of the trumpet-playing alligator?",
+    answer: "Louis",
+  },
+  {
+    question: "Who is the main antagonist in 'The Little Mermaid'?",
+    answer: "Ursula",
+  },
+  {
+    question:
+      "Which Disney character is a brave and determined toy cowboy in the 'Toy Story' franchise?",
+    answer: "Woody",
+  },
+  {
+    question: "What is the name of the talking snowman in 'Frozen'?",
+    answer: "Olaf",
+  },
+  {
+    question: "Who is the adventurous young boy in the movie 'Up'?",
+    answer: "Russell",
+  },
+  {
+    question:
+      "Which Disney character is a charismatic and mischievous pirate fox in 'Zootopia'?",
+    answer: "Nick Wilde",
+  },
+  {
+    question: "In Aladdin, what sort of pet is Rajah?",
+    answer: "Tiger",
+  },
+];
+
+// Export the array if using in a module-based environment
+export default triviaList;
